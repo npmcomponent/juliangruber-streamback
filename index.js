4fb860978b5cc483e320271304d8606ce6f0c0fd
Julian Gruber <julian@juliangruber.com>
2012-10-05 17:17:43 +0200

    refactorings

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-var Stream = require('stream');
-var Buffered = require('pause-stream');
+var Stream = require('pause-stream');
 
 module.exports = function() {
-  var s = new Stream;
-  s.readable = true;
+  var s = new Stream();
 
-  var buffered = Buffered();
-  s.pipe(buffered);
-
-  buffered.feed = function() {
+  s.feed = function() {
     return function() {
       if (s.stopped) return;
       var args = [].slice.call(arguments);
-      var data = ['data'];
-      if (args.length == 1) data.push(args[0]);
-      if (args.length > 1) data.push(args);
-      s.emit.apply(s, data);
+      if (args.length > 1) args = [args];
+      s.write.apply(s, args);
     }
   }
-  buffered.stop = function() {
+  s.stop = function() {
     s.stopped = true;
   }
-  buffered.start = function() {
+  s.start = function() {
     s.stopped = false;
   }
-  return buffered;
+  return s;
 };
